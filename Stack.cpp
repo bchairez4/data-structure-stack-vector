001ead4a2cd7961f5855ed11db0526dd3c60c0b0
Brian Chairez <bchairez@Navii.local>
2023-04-12 10:04:26 -0700

    Adding existing project to Github

---
+++ Stack.cpp
@@ -0,0 +1,85 @@
+#include "Stack.h"
+
+/*****************************************************************************
+    Constructors/ Destructor
+*****************************************************************************/
+// Time Complexity: O(1).
+template <class T>
+Stack<T>::Stack() {}
+
+// Fill Constructor
+// Time Complexity: O(N), where N is the given capacity.
+template <class T>
+Stack<T>::Stack(const int& capacity, const T& data) {
+    if (capacity <= 0) {
+        std::vector<T> temp(1, data);
+        stack_.swap(temp);
+        return;
+    }
+
+    std::vector<T> temp_stack(capacity, data);
+    stack_.swap(temp_stack);
+}
+
+// Copy Constructor
+// Time Complexity: O(N), where N is the number of objects in other.
+template <class T>
+Stack<T>::Stack(const Stack<T>& other) {
+    std::vector<T> temp_stack(other.stack_);
+    stack_.swap(temp_stack);
+}
+
+// Time Complexity: O(N), where N is the number of objects in the stack.
+template <class T>
+Stack<T>::~Stack() {
+    stack_.clear();
+}
+
+/*****************************************************************************
+    Operator Overloading
+*****************************************************************************/
+// Time Complexity: O(N), where N is the number of objects in other
+template <class T>
+T& Stack<T>::operator=(const Stack<T>& other) {
+    std::vector<T> temp_stack(other.stack_);
+    stack_.swap(temp_stack);
+}
+
+/*****************************************************************************
+    Data Modification
+*****************************************************************************/
+// Time Complexity: O(1) amortized. Resizing may occur
+template <class T>
+void Stack<T>::push(const T& data) {
+    stack_.push_back(data); 
+}
+
+// Time Complexity: O(1)
+template <class T>
+void Stack<T>::pop() {
+    if (!empty()) {
+        stack_.pop_back();
+    }
+}
+
+// WARNING: UNDEFINED BEHAVIOUR IF STACK IS EMPTY. USE EMPTY() CHECK BEFORE USE OF TOP()
+// Time Complexity: O(1)
+template <class T>
+T& Stack<T>::top() {
+    return stack_.back();
+}
+
+/*****************************************************************************
+    Data Observation
+*****************************************************************************/
+// Time Complexity: O(1)
+template <class T>
+bool Stack<T>::empty() const {
+    return stack_.empty();
+}
+
+// Time Complexity: O(1)
+template <class T>
+int Stack<T>::size() const {
+    return stack_.size();
+}
---
+++ Stack.h
@@ -0,0 +1,35 @@
+/*****************************************************************************
+*    Brian Chairez                                                           *
+*    Goal: std::vector implementation of std::stack                          *
+*    Advantages:                                                             *
+*       -Fast operations: O(1) amortized push(); O(1) pop(), and top()       *
+*       -Built in vector size and functions are helpful                      *
+*    Limitations:                                                            *
+*       -Can only remove from the top of the stack one at a time             *
+*       -No way to access anything in the middle or beginning of the stack   *
+*****************************************************************************/
+
+#ifndef STACK_H
+#define STACK_H
+
+#include <vector>
+
+// LIFO
+template <class T>
+class Stack {
+    private:
+        std::vector<T> stack_;
+    public:
+        Stack();
+        Stack(const int& capacity, const T& data);
+        Stack(const Stack<T>& other);
+        ~Stack();
+        T& operator=(const Stack<T>& other);
+        void push(const T& data);
+        void pop();
+        T& top();
+        bool empty() const;
+        int size() const;
+};
+
+#endif
